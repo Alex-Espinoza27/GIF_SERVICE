a0f792ac71236548263bfb42a7021deb3a100cb7
Yanely Dolores <u20211d914@upc.edu.pe>
2024-04-26 17:50:28 -0500

    Query

--- src/main/java/pe/edu/upc/giftservice/repositories/IEntrepreneurshipRepository.java
+++ src/main/java/pe/edu/upc/giftservice/repositories/IEntrepreneurshipRepository.java
@@ -10,21 +10,38 @@ import java.util.List;
 
 @Repository
 public interface IEntrepreneurshipRepository extends JpaRepository<Entrepreneurship, Long> {
-    @Query("SELECT pd.namePersonalizedDetail, COUNT(p) AS usageCount " +
-            "FROM Product p " +
-            "JOIN p.entrepreneurship e " +
-            "JOIN p.personalizedDetail pd " +
-            "WHERE e.id = :entrepreneurshipId " +
-            "GROUP BY pd " +
-            "ORDER BY usageCount DESC")
+    @Query(value = "SELECT \n" +
+            "    pd.name_personalized_detail AS Personalization,\n" +
+            "    COUNT(p.id_product) AS UsageCount \n" +
+            "FROM \n" +
+            "    entrepreneurships e \n" +
+            "INNER JOIN \n" +
+            "    product p ON p.entrepreneurship_id = e.id \n" +
+            "INNER JOIN \n" +
+            "    personalized_detail pd ON p.personalized_detail_id = pd.id_personalized_detail \n" +
+            "WHERE \n" +
+            "    e.id = :entrepreneurshipId \n" +
+            "GROUP BY \n" +
+            "    pd.name_personalized_detail \n" +
+            "ORDER BY \n" +
+            "    UsageCount DESC \n" +
+            "LIMIT 3", nativeQuery = true)
     List<String[]> findTop3PersonalizationsByEntrepreneurshipId(@Param("entrepreneurshipId") Long entrepreneurshipId, Pageable pageable);
 
-    @Query("SELECT p.nameProduct, COUNT(r) AS reviewCount " +
-            "FROM Reviews r " +
-            "JOIN r.product p " +
-            "JOIN p.entrepreneurship e " +
-            "WHERE e.id = :entrepreneurshipId " +
-            "GROUP BY p " +
-            "ORDER BY reviewCount DESC")
+    @Query(value ="SELECT\n" +
+            "   p.name_product AS ProductName,\n" +
+            "   COUNT(r.id_reviews) AS ReviewCount \n" +
+            "FROM \n" +
+            "   reviews r \n" +
+            "INNER JOIN \n" +
+            "   product p ON r.product_id = p.id_product \n" +
+            "INNER JOIN \n" +
+            "   entrepreneurships e ON p.entrepreneurship_id = e.id \n" +
+            "WHERE \n" +
+            "   e.id = 1 \n" +
+            "GROUP BY \n" +
+            "   p.name_product \n" +
+            "ORDER BY \n" +
+            "   ReviewCount DESC", nativeQuery = true)
     List<String[]> quantityReviewsByProduct(@Param("entrepreneurshipId") Long entrepreneurshipId);
 }
